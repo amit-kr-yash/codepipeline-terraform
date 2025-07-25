535b76438f8a0944211ff7e5379065564a1a39d0
AMIT KUMAR YASH <codewithyash10@gmail.com>
2025-07-25 09:05:28 +0530

    Update codepipeline_test.go

--- test/codepipeline_test.go
+++ test/codepipeline_test.go
@@ -2,9 +2,15 @@ package test
 
 import (
 	"fmt"
+	"strings"
 	"testing"
+	"time"
 
-	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/codepipeline"
+
+	"github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
@@ -13,39 +19,74 @@ import (
 func TestTerraformAwsCodePipelineProduction(t *testing.T) {
 	t.Parallel()
 
-	// Give this project a unique name so we can run tests in parallel
-	uniqueId := random.UniqueId()
+	uniqueId := strings.ToLower(random.UniqueId())
 	projectName := fmt.Sprintf("test-prod-app-%s", uniqueId)
-	awsRegion := "ap-south-1" // The test will run in this region
+	awsRegion := "ap-south-1"
 
-	// Configure the options for Terraform
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../",
 		Vars: map[string]interface{}{
 			"project_name":      projectName,
 			"aws_region":        awsRegion,
-			"github_repo_owner": "gruntwork-io",
-			"github_repo_name":  "terratest-hello-world",
+			"github_repo_owner": "ameet56",
+			"github_repo_name":  "terraform-demo",
+			"github_branch":     "main",
 		},
-		// NOTE: We are NOT using the WorkspaceName feature in this simplified version
-		// to ensure compatibility with your Go environment.
 	}
 
-	// At the end of the test, run `terraform destroy` to clean up all resources.
 	defer terraform.Destroy(t, terraformOptions)
 
-	// Run `terraform init` and `terraform apply`.
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Get the website URL output from Terraform
 	websiteURL := terraform.Output(t, terraformOptions, "website_url")
-	assert.NotEmpty(t, websiteURL, "Website URL output is empty")
+	connectionArn := terraform.Output(t, terraformOptions, "codestar_connection_arn")
+	pipelineName := terraform.Output(t, terraformOptions, "codepipeline_name")
 
-	// Validate that the correct number of EC2 instances were launched by the ASG.
-	// We find them by looking for the tag we assigned in the Launch Template.
-	// This is a stable function that should work with your library version.
-	instanceIds := aws.GetEc2InstanceIdsByTag(t, awsRegion, "Name", fmt.Sprintf("%s-dev-web-server", projectName))
-	assert.Equal(t, 2, len(instanceIds), "Expected to find exactly 2 EC2 instances with the correct tag")
+	t.Logf("PAUSING FOR 1 MINUTE to allow for manual CodeStar connection approval.")
+	t.Logf("Please go to the AWS Console and approve the connection with ARN: %s", connectionArn)
+	time.Sleep(1 * time.Minute)
 
-	t.Logf("Successfully validated that 2 instances were created for the ASG.")
+	// Start pipeline execution
+	err := startPipelineExecution(t, awsRegion, pipelineName)
+	assert.NoError(t, err)
+	t.Logf("Manually started pipeline %s. Waiting for deployment to complete...", pipelineName)
+
+	// FIX: Increased the wait time from 5 minutes to 15 minutes (900 seconds)
+	// to allow the slow, zero-downtime rolling deployment to complete.
+	t.Logf("Waiting for 18 minutes for the deployment to finish...")
+	time.Sleep(900 * time.Second)
+
+	// Optional: Log raw HTTP output for debugging
+	status, body := http_helper.HttpGet(t, websiteURL, nil)
+	t.Logf("HTTP Status: %d", status)
+	t.Logf("HTTP Body: %s", body)
+
+	// Validate the website is accessible via the Load Balancer
+	http_helper.HttpGetWithRetryWithCustomValidation(
+		t,
+		websiteURL,
+		nil,
+		30,
+		10*time.Second,
+		func(statusCode int, body string) bool {
+			return statusCode == 200
+		},
+	)
+
+	t.Logf("Successfully validated that the website is live.")
+}
+
+func startPipelineExecution(t *testing.T, awsRegion string, pipelineName string) error {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(awsRegion),
+	})
+	if err != nil {
+		return err
+	}
+	svc := codepipeline.New(sess)
+
+	_, err = svc.StartPipelineExecution(&codepipeline.StartPipelineExecutionInput{
+		Name: aws.String(pipelineName),
+	})
+	return err
 }
